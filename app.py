171793760a3087742a87b41614e48ed6a7c059d5
Lex <55722511+LexLeach@users.noreply.github.com>
2020-09-28 00:59:53 -0400

    added schemas to create bookmarks and added 4 bookmarks to the database to start with

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ db = PostgresqlDatabase('Bookmarks', user='lexleach', password='', host='localho
 
 db.connect()
 
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Bookmark(BaseModel):
+    name = CharField()
+    link = CharField()
+    date_added = DateField()
+
+db.drop_tables([Bookmark])
+db.create_tables([Bookmark])
+
+google = Bookmark(name='Google', link='https://www.google.com', date_added=datetime.datetime.now()).save()
+reddit = Bookmark(name='Reddit', link='https://www.reddit.com', date_added=datetime.datetime.now()).save()
+steam = Bookmark(name='Steam', link='https://store.steampowered.com/', date_added=datetime.datetime.now()).save()
+humble_bundle = Bookmark(name='Humble Bundle', link='https://www.humblebundle.com/', date_added=datetime.datetime.now()).save()
+
